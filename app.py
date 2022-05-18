32ad6a472fe6d433f4174b1ed77a7f5f8f08305f
Grégoire Deveaux <gregoire.deveaux@backmarket.com>
2022-05-18 10:54:11 +0200

    dynamic mappings tests

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ app = flask.Flask(__name__)
 
 EXTERNAL_SERVICE_URL = os.environ["EXTERNAL_SERVICE_URL"]
 
+
 @app.route("/multiply-external-service")
 def multiply_external_service():
     factor = int(flask.request.args.get("factor", "1"))
-    external_service_value = requests.get(f"{EXTERNAL_SERVICE_URL}/get-value").json()["value"]
+    external_service_value = requests.get(f"{EXTERNAL_SERVICE_URL}/get-value").json()[
+        "value"
+    ]
     return {"result": factor * external_service_value}
--- integration-tests/test_my_service.py
+++ integration-tests/test_my_service.py
@@ -2,11 +2,27 @@ import os
 import requests
 
 MY_SERVICE_URL = os.environ["MY_SERVICE_URL"]
+EXTERNAL_SERVICE_URL = os.environ["EXTERNAL_SERVICE_URL"]
+
 
 def test_my_service():
-    factor = 3
+    set_external_service_response({"status": 200, "jsonBody": {"value": 3}})
+    my_service_factor = 3
 
-    response = requests.get(f"{MY_SERVICE_URL}/multiply-external-service?factor={factor}")
+    response = requests.get(
+        f"{MY_SERVICE_URL}/multiply-external-service?factor={my_service_factor}"
+    )
 
     assert response.status_code == 200
     assert response.json()["result"] == 9
+
+
+def set_external_service_response(response):
+    response = requests.post(
+        f"{EXTERNAL_SERVICE_URL}/__admin/mappings",
+        json={
+            "request": {"method": "GET", "url": "/get-value"},
+            "response": response,
+        },
+    )
+    assert response.status_code == 201

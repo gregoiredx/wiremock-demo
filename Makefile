57be6a9a482eb227b3e67ca3dccf3f8f380f343a
Grégoire Deveaux <ggdx@laposte.net>
2022-05-18 09:28:00 +0200

    --amend

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
+.PHONY: up
 up:
 	docker compose up --build -d external-service my-service
+
+.PHONY: integration-tests
 integration-tests:
 	docker-compose build integration-tests
 	docker-compose run --rm integration-tests
---
+++ integration-tests/test_my_service.py
@@ -0,0 +1,12 @@
+import os
+import requests
+
+MY_SERVICE_URL = os.environ["MY_SERVICE_URL"]
+
+def test_my_service():
+    factor = 3
+
+    response = requests.get(f"{MY_SERVICE_URL}/multiply-external-service?factor={factor}")
+
+    assert response.status_code == 200
+    assert response.json()["result"] == 9
